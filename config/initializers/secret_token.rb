f2590dc0885638b6c0040fd151a86f1e0599f96d
Aaron Swensen <aaron.swensen@gmail.com>
2013-05-17 11:01:25 -0600

    Rails app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archipelago::Application.config.secret_token = '730df251dea90f3fc3c210d47418e3c7e7359e229617a7f68999d54cec508da13902c0f814989a6fa5d55483b1c1b25afdc5ea691df882d5136207ecdfa58dad'
